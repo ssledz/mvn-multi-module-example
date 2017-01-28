9c46a337d8fd4cac7fa87f595258c6089a5b637f
Sławomir Śledź <slawomir.sledz@gmail.com>
2017-01-29 00:23:03 +0100

    bzz

---
+++ api/src/main/java/pl/softech/example/mvn/App.java
@@ -0,0 +1,13 @@
+package pl.softech.example.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
