b72648e8facecf8cc088b11bc5db714d2e764a72
Sławomir Śledź <slawomir.sledz@iiit.pl>
2017-01-30 17:07:39 +0100

    bzz

---
+++ echo-service/impl-1/src/main/java/pl/softech/example/mvn/impl1/Main.java
@@ -0,0 +1,16 @@
+package pl.softech.example.mvn.impl1;
+
+/**
+ * @author ssledz
+ * @since 30.01.17
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleEchoService simpleEchoService = new SimpleEchoService();
+        for (String message : args) {
+            simpleEchoService.echo(message);
+        }
+    }
+
+}
